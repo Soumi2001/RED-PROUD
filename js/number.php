c185c088e6ee3abb8d2ce9e50d84908b30305192
Soumi2001 <108082881+Soumi2001@users.noreply.github.com>
2023-03-31 12:18:19 +0530

    Add files via upload

---
+++ js/number.php
@@ -0,0 +1,28 @@
+<?php 
+$sql1 ="SELECT id from tblblooddonars ";
+$query1 = $dbh -> prepare($sql1);;
+$query1->execute();
+$results1=$query1->fetchAll(PDO::FETCH_OBJ);
+$regbd=$query1->rowCount();
+?>
+<script type="text/javascript">
+    var a = <?php echo json_encode($regbd); ?>;
+    const counterAnim = (qSelector, start = 0, end, duration = 1000) => {
+    const target = document.querySelector(qSelector);
+    let startTimestamp = null;
+    const step = (timestamp) => {
+     if (!startTimestamp) startTimestamp = timestamp;
+     const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+     target.innerText = Math.floor(progress * (end - start) + start);
+     if (progress < 1) {
+      window.requestAnimationFrame(step);
+     }
+    };
+    window.requestAnimationFrame(step);
+   };
+   //#endregion - end of - number counter animation
+   
+   document.addEventListener("DOMContentLoaded", () => {
+    counterAnim("#count1", 50, a, 100);
+   });
+</script>
